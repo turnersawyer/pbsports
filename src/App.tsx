43b7aff3ccc3b40e05886d3cb50b650ee0d9d4c3
turnersawyer <60157360+turnersawyer@users.noreply.github.com>
2022-04-27 21:54:47 -0500

    Added blog template

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomeHeader from './components/HomeHeader';
+import { Container, createTheme, CssBaseline, Grid, ThemeProvider } from '@mui/material';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import FeaturedPost from './components/FeaturedPost';
+import MainFeaturedPost from './components/MainFeaturedPost';
+import { sections, title } from './constants/HeaderConstants'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import post1 from './posts/blog-post.1.md';
+import post2 from './posts/blog-post.2.md';
+import post3 from './posts/blog-post.3.md';
+
+
+const mainFeaturedPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imageText: 'main image description',
+  linkText: 'Continue reading…',
+};
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageLabel: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageLabel: 'Image Text',
+  },
+];
+
+const posts = [post1, post2, post3];
+
+const sidebar = {
+  title: 'About',
+  description:
+    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+  archives: [
+    { title: 'March 2020', url: '#' },
+    { title: 'February 2020', url: '#' },
+    { title: 'January 2020', url: '#' },
+    { title: 'November 1999', url: '#' },
+    { title: 'October 1999', url: '#' },
+    { title: 'September 1999', url: '#' },
+    { title: 'August 1999', url: '#' },
+    { title: 'July 1999', url: '#' },
+    { title: 'June 1999', url: '#' },
+    { title: 'May 1999', url: '#' },
+    { title: 'April 1999', url: '#' },
+  ],
+  social: [
+    { name: 'Instagram', icon: InstagramIcon, url: 'https://twitter.com/PBsportsnews' },
+    { name: 'Twitter', icon: TwitterIcon, url: 'https://twitter.com/PBsportsnews' },
+    { name: 'Facebook', icon: FacebookIcon, url: 'https://twitter.com/PBsportsnews' },
+  ],
+};
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <HomeHeader title={title} sections={sections} />
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Grid container spacing={5} sx={{ mt: 3 }}>
+            <Main title="From the firehose" posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+      <Footer
+        title="PBS"
+        description="Sports news by the people, for the people"
+      />
+    </ThemeProvider>
   );
 }
 
---
+++ src/constants/HeaderConstants.ts
@@ -0,0 +1,13 @@
+export const sections = [
+  { title: "NFL", url: "nfl" },
+  { title: "NBA", url: "nba" },
+  { title: "MLB", url: "mlb" },
+  { title: "NHL", url: "nhl" },
+  { title: "CFB", url: "cfb" },
+  { title: "CBB", url: "CBB" },
+  { title: "Golf", url: "golf" },
+  { title: "Soccer", url: "soccer" },
+  { title: "F1", url: "f1" }
+]
+
+export const title = "Power Bottom Sports"
